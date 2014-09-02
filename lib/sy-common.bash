0819dc8985b64091f53e3c878716b2ff7c2a5a8e
Gerhard Lichtner <gerhard@linux-5es9.site.suska.org>
2014-09-02 17:17:22 +0200

    Started with adaptions to opensuse

--- lib/sy-common.bash
+++ lib/sy-common.bash
@@ -7,10 +7,15 @@ function syve_section {
   logger syve_section $*
 }
 
+has_apt=$(which apt-get)
+
 function apt_install {
   echo
   echo Installing $*
   logger SyveInstalling $*
-  sudo apt-get --yes install $*
+  if [ $has_apt ] ;then
+    sudo apt-get --yes install $*
+  else
+    sudo -n zypper install $*
+  fi  
 }
-
