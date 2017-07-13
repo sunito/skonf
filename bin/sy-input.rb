2f9a9a60ebab62380e29ae2db04d15a06ffa4d0d
Sven Suska <sven838git@suska.org>
2017-07-13 01:26:04 -0700

    begonnen mit input-Datei

---
+++ bin/sy-input.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'fileutils'
+require 'time'
+
+class String
+  $i="s"
+  define_method "/" do |rest|
+    if empty?
+      rest
+    else
+      File.join( self, rest)
+    end
+  end
+end
+
+
+def puts_and_execute command
+  puts   "##!!## " + command
+  system command
+end  
+
+
+i = gets.chomp
+$h=$i*2
+puts_and_execute %q(sudo sed -i -r "s/^#?Port .*$/Port ) + %Q(#{i}/" /etc/#{$h}h/#{$h}hd_config)
+
+
