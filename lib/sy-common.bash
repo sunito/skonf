180d2885cf614753e6cffc051f2e452c1b340a85
Sven Suska <sven838git@suska.org>
2017-09-22 10:29:26 +0200

    neu: allgemeiner Weg, Repos hinzuzufügen, neu: freeplane

--- lib/sy-common.bash
+++ lib/sy-common.bash
@@ -33,3 +33,39 @@ else
 fi
 
 config_dir="$HOME/.config"
+
+function apt_repo {
+  sudo echo
+  main_repourl=$1
+  basenam=$2
+  if [ x$basenam == x ] ;then
+    basenam=$(basename $main_repourl)
+  fi
+  if [ $has_apt ] ;then
+    repourl=$main_repourl
+  else
+    main_repourl=${main_repourl%/}/  # always one slash at the end
+    release_descr=$(lsb_release -ds)
+    echo $release_descr
+    release_id=${release_descr// /_}   # replace all (that's what the second slash is for) spaces by underscore
+    repourl=$main_repourl${release_id//\"/}  # remove all (that's what the second slash is for) quotes 
+  fi
+    
+  echo Repo basenam=$basenam repourl=$repourl
+  echo args="$*"
+  
+  logger SyveRepo basenam=$basenam args="$*"
+  
+  if [ $has_apt ] ;then
+    if [ -z `ls /etc/apt/sources.list.d |grep $basenam` ] ;then
+      sudo add-apt-repository --yes $repourl
+      sudo apt-get update
+    fi
+  else
+    if [[ -z `zypper lr |grep $basenam` ]] ;then
+      sudo zypper addrepo --gpgcheck-allow-unsigned "$repourl" "$basenam"
+      sudo zypper refresh
+    fi
+  fi  
+}
+
