85dea35220abbd67f210601b9db8eec4bdb5238a
leoti61 <skonf-leoti61@suska.org>
2017-09-24 08:33:07 +0200

    apt_ppa primitiv aus apt_repo, minecraft-installer für Ubuntu

--- lib/sy-common.bash
+++ lib/sy-common.bash
@@ -50,22 +50,47 @@ function apt_repo {
     release_id=${release_descr// /_}   # replace all (that's what the second slash is for) spaces by underscore
     repourl=$main_repourl${release_id//\"/}  # remove all (that's what the second slash is for) quotes 
   fi
-    
-  echo Repo basenam=$basenam repourl=$repourl
-  echo args="$*"
-  
-  logger SyveRepo basenam=$basenam args="$*"
+      
   
   if [ $has_apt ] ;then
+    echo NOT Repo basenam=$basenam repourl=$repourl
+  else
+    echo Repo basenam=$basenam repourl=$repourl
+    logger SyveRepo basenam=$basenam args="$*"
+    if [[ -z `zypper lr |grep $basenam` ]] ;then
+      sudo zypper addrepo --gpgcheck-allow-unsigned "$repourl" "$basenam"
+      sudo zypper refresh
+    fi
+  fi  
+}
+
+function apt_ppa {
+  sudo echo
+  main_repourl=$1
+  basenam=$2
+  if [ x$basenam == x ] ;then
+    basenam=$(basename $main_repourl)
+  fi
+  if [ $has_apt ] ;then
+    repourl=$main_repourl
+  else
+    main_repourl=${main_repourl%/}/  # always one slash at the end
+    release_descr=$(lsb_release -ds)
+    echo $release_descr
+    release_id=${release_descr// /_}   # replace all (that's what the second slash is for) spaces by underscore
+    repourl=$main_repourl${release_id//\"/}  # remove all (that's what the second slash is for) quotes 
+  fi
+    
+  
+  if [ $has_apt ] ;then
+    echo Repo basenam=$basenam repourl=$repourl
+    logger SyveRepo basenam=$basenam args="$*"
     if [ -z `ls /etc/apt/sources.list.d |grep $basenam` ] ;then
       sudo add-apt-repository --yes $repourl
       sudo apt-get update
     fi
   else
-    if [[ -z `zypper lr |grep $basenam` ]] ;then
-      sudo zypper addrepo --gpgcheck-allow-unsigned "$repourl" "$basenam"
-      sudo zypper refresh
-    fi
+    echo NOT Repo basenam=$basenam repourl=$repourl
   fi  
 }
 
