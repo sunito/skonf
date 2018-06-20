e2b3592aebb80c1f43bdda4e5f3a3b4806dbde8d
Sven Suska <immensly83@suska.org>
2018-06-20 10:47:10 +0200

    neu: Erkennung leerer Mails

--- bin/raschgetan.rb
+++ bin/raschgetan.rb
@@ -4,7 +4,7 @@
 require 'fileutils'
 require 'time'
 
-MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET = 120
+MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET = 150 # 2.5 Minuten -- schneller überweiset man nicht
 TIMEOUT_SEKUNDEN = 10 # auch 8sek hat nicht gereicht f GLS
 #TIMEOUT_SEKUNDEN = 8 # 6 wäre für GLS zu wenig
 
@@ -27,19 +27,22 @@ class TanMails
   def initialize dir
     @dir = dir
   end
-  
+
   def neueste
-    alle_dateinamen = Dir[@dir/"*/*"]  # Dir[@dir/"cur/*"] + 
+    alle_dateinamen = Dir[@dir/"*/*"]  # Dir[@dir/"cur/*"] +
     neueste_datei = alle_dateinamen.sort_by{|dn| dont { print File.mtime(dn); puts " <-- "+dn};   File.mtime(dn)}.last
-    #p File.mtime(neueste_datei)    
+    #p File.mtime(neueste_datei)
     #p neueste_datei
     neueste_datei
   end
-  
+
   def extrahiere_tan text
     [
+      # ToDo: weitere Erkenner (z.B. generisch), da aktuell alle nicht-Standard-Tans als "Leer" klassifiziert werden
       / lautet: ([\w\d]{6})/, # Postb
+      /\+Postba()/,           # Postb leer
       / lautet ([\d]{6}),/,   # GLS standard
+      /\+GLS TAN()/,            # GLS leer
       / TAN ([\w\d]{6})\./     # 3dsecure-cardprocess.de (Mastercard Securcode)
     ].find do |re|
       if re =~ text
@@ -48,27 +51,30 @@ class TanMails
     end
     nil
   end
-  
+
   def status
     neu_text = File.read neueste
     tan = extrahiere_tan neu_text
-    # ToDo: Erkennung leerer SMS-Mails
+   if tan.empty? # erster Ansatz zur Erkennung leerer SMS-Mails
+      [false, "FEHLER!! \n Die Tan-SMS kam leer an.\n Bitte NEUE Tan anfordern!"]
+   else
     if tan
       zeit_str = neu_text.scan(/^Date: (.+)$/).first.first
       zeit = Time.parse(zeit_str) if zeit_str
       if zeit.nil?
-        [false, "keine Zeit in der Mail gefunden!! (Tan: #{tan}"]
+        [true, "Ungewöhnliche Tan-Mail erhalten:\n keine Zeit in der Mail gefunden!! (Tan: #{tan}"]
       else
         erfolg =  (Time.now - zeit < MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET)
-        [ erfolg, (erfolg ? '' : "Keine neue Tan-Mail \n  Alte ") +
+        [ erfolg, (erfolg ? '' : "KEINE NEUE Tan-Mail \n\n  Alte ") +
           "Tan: #{tan}      Zeit: #{zeit.strftime('%b-%d. %T')}"
         ]
       end
     else
       [false, "Keine aktuelle Tan-Mail gefunden"]
     end
+   end
   end
-  
+
   def warte wartedauer=TIMEOUT_SEKUNDEN
     anf_zeit = Time.now
     text = nil
