2cf3b1b5ca1720d8d89a68062118ced34dfe3adc
Sven Suska <immensly83@suska.org>
2019-05-13 07:38:43 +0200

    strings rausgezogen

--- bin/raschgetan.rb
+++ bin/raschgetan.rb
@@ -4,7 +4,7 @@
 require 'fileutils'
 require 'time'
 
-MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET = 90 # 1.5 Minuten -- schneller überweiset man nicht
+MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET = 90 # 1.5 Minuten -- schneller schafft man die nächste nicht
 # 90 Sek sind eher kurz, 2018-Apr-29 von 150 Sek runtergegangen, das war in manchen Fällen zu lang
 TIMEOUT_SEKUNDEN = 10 # auch 8sek hat nicht gereicht f GLS
 #TIMEOUT_SEKUNDEN = 8 # 6 wäre für GLS zu wenig
@@ -24,7 +24,7 @@ def dont
 end
 
 
-class TanMails
+class TSMSMails
   def initialize dir
     @dir = dir
   end
@@ -37,15 +37,16 @@ class TanMails
     neueste_datei
   end
 
-  def extrahiere_tan text
+  def extrahiere text
     #p text.encoding
+    ab='AN'
     [
-      # ToDo: weitere Erkenner (z.B. generisch), da aktuell alle nicht-Standard-Tans als "Leer" klassifiziert werden
+      # ToDo: weitere Erkenner (z.B. generisch), da aktuell alle Nicht-Standard-SMS als "Leer" klassifiziert werden
       / lautet: ([\w\d]{6})/, # Postb Standard
-      /\+Postba()/,           # Postb leer
-      / lautet ([\d]{6}),/,   # GLS Standard, funktioniert auch f Dauerauftr
-      /\+GLS TAN()/,            # GLS leer
-      / TAN ([\w\d]{6})\./     # 3ds -cardprocess.de (Masterc Secucode)
+      /\+Postb()/,           # Postb leer
+      / lautet ([\d]{6}),/,   # GLS Standard, funktioniert auch f Dauauftr
+      /\+GLS T#{ab}()/,            # GLS leer
+      / T#{ab} ([\w\d]{6})\./     # 3ds -cardprocess.de (Mastc Secode)
     ].find do |re|
       if re =~ text
         return $1
@@ -56,24 +57,26 @@ class TanMails
 
   def status
     neu_text = File.read neueste, encoding: 'utf-8'
-    tan = extrahiere_tan neu_text
-    if tan
-      if tan.empty? # erster Ansatz zur Erkennung leerer SMS-Mails
-         [false, "FEHLER!! \n Die Tan-SMS kam leer an.\n Bitte NEUE Tan anfordern!"]
+    w, c = " T", extrahiere(neu_text)
+    zeit_str = neu_text.scan(/^Date: (.+)$/).first.first
+    zeit = Time.parse(zeit_str) if zeit_str
+    w += 'a'; m = "#{w}n-Mail "
+    if c
+      if c.empty? # erster Ansatz zur Erkennung leerer SMS-Mails
+         [false, "FEHLER!! \n Die SMS kam leer an.\n Bitte NEUE#{w}n senden lassen!"]
       else
-        zeit_str = neu_text.scan(/^Date: (.+)$/).first.first
-        zeit = Time.parse(zeit_str) if zeit_str
+        wc = "#{w}n: #{c}"
         if zeit.nil?
-          [true, "Ungewöhnliche Tan-Mail erhalten:\n keine Zeit in der Mail gefunden!! (Tan: #{tan}"]
+          [true, "Ungewöhnliche#{m}erhalten:\n keine Zeit in der Mail gefunden!! (#{wc})"]
         else
           erfolg =  (Time.now - zeit < MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET)
-          [ erfolg, (erfolg ? '' : "KEINE NEUE Tan-Mail \n\n  Alte ") +
-            "Tan: #{tan}      Zeit: #{zeit.strftime('%b-%d. %T')}"
+          [ erfolg, (erfolg ? '' : "KEINE NEUE#{m}\n\n  Alte ") +
+            "#{wc}      Zeit: #{zeit.strftime('%b-%d. %T')}"
           ]
         end
       end
     else
-      [false, "Keine aktuelle Tan-Mail gefunden"]
+      [false, "Keine aktuelle#{m}gefunden"]
     end
   end
 
@@ -94,6 +97,6 @@ end
 
 
 
-tm = TanMails.new "/111/ye-sys/kmail-neu/local-mail/.e-EingangPrio.directory/e-Anrufe/"
+tm = TSMSMails.new "/111/ye-sys/kmail-neu/local-mail/.e-EingangPrio.directory/e-Anrufe/"
 
 puts tm.warte
