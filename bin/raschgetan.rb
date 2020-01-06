8a7b63cdc3450f62fc72292fc1bb9043bd283e96
Sven Suska <immensly83@suska.org>
2020-01-06 08:30:20 +0100

    doch lieber länger warten (9sek)

--- bin/raschgetan.rb
+++ bin/raschgetan.rb
@@ -6,7 +6,7 @@ require 'time'
 
 MAX_SEKUNDEN_MAIL_NOCH_ALS_NEU_GEWERTET = 120 # 2 Minuten <2019,Mai30>
 # 1.5 Minuten -- schneller schafft man die nächste nicht. 90 Sek sind eher kurz, 2018Apr von 150 Sek runtergegangen, das war in manchen Fällen zu lang
-TIMEOUT_SEKUNDEN = 7
+TIMEOUT_SEKUNDEN = 9
 # War mal 10, das reicht immer, 8sek hat nicht gereicht f Gls
 #TIMEOUT_SEKUNDEN = 8 # 6 wäre für Gls zu wenig
 
