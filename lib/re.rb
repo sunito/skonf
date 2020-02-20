edad3e3183b522a3a97e7fccef9b24759c0f889d
Sven Suska <immensly83@suska.org>
2020-02-20 17:05:28 +0100

    hinzu: re.rb als Include-Datei für adhoc-Taschenrechner

---
+++ lib/re.rb
@@ -0,0 +1,17 @@
+
+def sind x
+  sin( x / 180.0 * Math::PI )
+end
+
+def asind x
+  asin(x) / Math::PI * 180
+end
+
+def cosd x
+  cos( x / 180.0 * Math::PI )
+end
+
+def acosd x
+  acos(x) / Math::PI * 180
+end
+
